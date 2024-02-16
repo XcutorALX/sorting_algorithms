0cf73d4c919ea2a72cd3ec45075c3e6a898a8631
Xcutor <Lekanabass50@gmail.com>
2024-02-16 02:18:39 -0600

    task done

---
+++ 3-quick_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * quick_sort_helper - helper function for the quick sort function
+ *
+ * @array: an array of integers
+ * @size: the size of the array
+ * @original: the original array
+ * @totalSize: the size of the original array
+ */
+
+void quick_sort_helper(int *array, size_t size,
+		int *original, size_t totalSize)
+{
+	size_t i, j;
+	int temp, pivot;
+
+	if (size < 2)
+		return;
+
+	pivot = array[size - 1];
+	j = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			temp = array[i];
+			if (i != j)
+			{
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(original, totalSize);
+			}
+			j++;
+		}
+	}
+	quick_sort_helper(array, j - 1, original, totalSize);
+	quick_sort_helper(&(array[j]), size - (j), original, totalSize);
+}
+
+/**
+ * quick_sort - sorts an array of integers using the quick sort algorithm
+ *
+ * @size: the size of the array
+ * @array: an array of integers
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_helper(array, size, array, size);
+}
