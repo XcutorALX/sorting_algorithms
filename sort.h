9d0d0cad8df5c23f9108bf49680a654cca189789
Xcutor <Lekanabass50@gmail.com>
2024-02-20 03:11:51 -0600

    task done

--- sort.h
+++ sort.h
@@ -22,7 +22,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_helper(int *array, size_t size, int *original, size_t totalSize);
+void quick_sort_helper(int *array, size_t size,
+		int *original, size_t totalSize);
 void shell_sort(int *array, size_t size);
 void insertion_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
